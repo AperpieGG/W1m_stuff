acc2d3add1b59c586d6e8fec102139031928d83b
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2025-08-08 16:18:44 +0100

    change directories

--- create_flats_W1m.py
+++ create_flats_W1m.py
@@ -5,27 +5,19 @@ from astropy.io import fits
 import numpy as np
 from utils_W1m import find_current_night_directory
 
-calibration_path = "/Users/u5500483/Downloads/DATA_MAC/CMOS/20231212/"
-base_path = "/Users/u5500483/Downloads/DATA_MAC/CMOS/20231212/"
-out_path = "/Users/u5500483/Downloads/DATA_MAC/CMOS/calibration_images/"
-outer_path = os.getcwd()
 
-
-def bias(out_path):
+def bias():
     """
     Create the master bias from the bias files.
 
     Parameters
     ----------
-    out_path : str
-        Path to the output directory.
-
     Returns
     -------
     numpy.ndarray
         Master bias.
     """
-    master_bias_path = os.path.join(out_path, 'master_bias.fits')
+    master_bias_path = os.path.join('.', 'master_bias.fits')
 
     if os.path.exists(master_bias_path):
         print('Found master bias')
@@ -35,21 +27,18 @@ def bias(out_path):
         return None
 
 
-def dark(out_path):
+def dark():
     """
     Create the master dark from the dark files.
 
     Parameters
     ----------
-    out_path : str
-        Path to the output directory.
-
     Returns
     -------
     numpy.ndarray
         Master dark.
     """
-    master_dark_path = os.path.join(out_path, 'master_dark.fits')
+    master_dark_path = os.path.join('.', 'master_dark.fits')
 
     if os.path.exists(master_dark_path):
         print('Found master dark')
@@ -59,18 +48,12 @@ def dark(out_path):
         return None
 
 
-def flat(outer_path, master_bias, master_dark, dark_exposure=10):
+def flat(master_dark, dark_exposure=10):
     """
     Create the master flat from the flat files.
 
     Parameters
     ----------
-    base_path : str
-        Base path for the directory.
-    outer_path : str
-        Path to the output directory.
-    master_bias : numpy.ndarray
-        Master bias.
     master_dark : numpy.ndarray
         Master dark.
     dark_exposure : int
@@ -83,12 +66,12 @@ def flat(outer_path, master_bias, master_dark, dark_exposure=10):
     """
     # Find and read the flat files
     # Find appropriate files for creating the master flat
-    evening_files = [f for f in glob.glob(os.path.join(outer_path, 'evening*.fits')) if
+    evening_files = [f for f in glob.glob(os.path.join('.', 'evening*.fits')) if
                      'HDR' in fits.getheader(f)['READMODE']]
 
     if not evening_files:
         # If evening files don't exist, use morning files
-        evening_files = [f for f in glob.glob(os.path.join(outer_path, 'morning*.fits')) if
+        evening_files = [f for f in glob.glob(os.path.join('.', 'morning*.fits')) if
                          'HDR' in fits.getheader(f)['READMODE']]
 
     if not evening_files:
@@ -102,10 +85,12 @@ def flat(outer_path, master_bias, master_dark, dark_exposure=10):
         # take only the first 21
         files = evening_files[:21]
 
-        cube = np.zeros((*master_bias.shape, len(files)))
+        cube = np.zeros((*master_dark.shape, len(files)))
         for i, f in enumerate(files):
             data, header = fits.getdata(f, header=True)
-            cube[:, :, i] = data - master_bias - master_dark * header['EXPTIME'] / dark_exposure
+            # for IMX571 we will not use master-bias, only dark
+            # cube[:, :, i] = data - master_bias - master_dark * header['EXPTIME'] / dark_exposure
+            cube[:, :, i] = data - master_dark * header['EXPTIME'] / dark_exposure
             cube[:, :, i] = cube[:, :, i] / np.average(cube[:, :, i])
 
         master_flat = np.median(cube, axis=2)
@@ -115,13 +100,13 @@ def flat(outer_path, master_bias, master_dark, dark_exposure=10):
 
         # Write the master flat with the copied header
         hdu = fits.PrimaryHDU(master_flat, header=header)
-        hdu.writeto(os.path.join(outer_path, 'master_flat.fits'), overwrite=True)
+        hdu.writeto(os.path.join('.', 'master_flat.fits'), overwrite=True)
 
-        hdul = fits.open(os.path.join(outer_path, 'master_flat.fits'), mode='update')
-        hdul[0].header['FILTER'] = 'NGTS'
+        hdul = fits.open(os.path.join('.', 'master_flat.fits'), mode='update')
+        hdul[0].header['FILTER'] = 'NONE'
         hdul.close()
 
-        print(f'Master flat saved to: {os.path.join(outer_path, "master_flat.fits")}')
+        print(f'Master flat saved to: {os.path.join(os.getcwd(), "master_flat.fits")}')
         return master_flat
 
 
@@ -129,11 +114,9 @@ def main():
     """
     Main function to create the master bias, dark, and flat.
     """
-    print(f'Using base path: {base_path}')
-
-    master_bias = bias(out_path)
-    master_dark = dark(out_path)
-    flat(outer_path, master_bias, master_dark)
+    print("Creating calibration images: master dark, and flat... NO BIAS !")
+    master_dark = dark()
+    flat(master_dark)
 
 
 if __name__ == '__main__':
