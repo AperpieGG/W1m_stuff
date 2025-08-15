dc6c8b8997881259efe4fced8a2aa76f6ba9df9f
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2025-08-15 11:21:06 +0100

    zip the files delete the png

--- zip_fits_W1m.py
+++ zip_fits_W1m.py
@@ -1,50 +1,49 @@
 #! /usr/bin/env python
 import os
 import bz2
+from astropy.io import fits
+
+# Words to exclude from science data filenames
+EXCLUDE_WORDS = ["bias", "flat", "evening", "morning", "phot", "rel"]
+
+
+def is_science_image(filename):
+    """Check if filename corresponds to a science image."""
+    lower_name = filename.lower()
+    return not any(word in lower_name for word in EXCLUDE_WORDS)
 
 
 def compress_fits_files(directory):
-    # Get a list of all .fits files in the directory
-    fits_files = [f for f in os.listdir(directory) if f.startswith('IMAGE') and f.endswith('.fits')]
+    """Compress only science FITS files into .bz2 format."""
+    fits_files = [
+        f for f in os.listdir(directory)
+        if f.endswith('.fits') and is_science_image(f)
+    ]
 
-    # Compress each .fits file into .fits.bz2 format
     for fits_file in fits_files:
         fits_path = os.path.join(directory, fits_file)
         fits_bz2_path = fits_path + '.bz2'
+
         with open(fits_path, 'rb') as source:
             with bz2.BZ2File(fits_bz2_path, 'wb') as target:
                 target.write(source.read())
-        print(f"{fits_file} compressed to {fits_bz2_path}")
 
-
-def delete_uncompressed_images(directory):
-    # Get a list of all uncompressed .fits files in the directory
-    uncompressed_files = [f for f in os.listdir(directory) if f.startswith('IMAGE') and f.endswith('.fits')]
-
-    # Delete each uncompressed .fits file
-    for file in uncompressed_files:
-        os.remove(os.path.join(directory, file))
-        print(f"{file} deleted.")
+        print(f"Compressed: {fits_file} → {fits_file}.bz2")
 
 
 def delete_png_files(directory):
-    # Get a list of all .png files in the directory
-    png_files = [f for f in os.listdir(directory) if f.startswith('IMAGE') and f.endswith('.png')]
-
-    # Delete each .png file
+    """Delete all PNG files in the directory."""
+    png_files = [f for f in os.listdir(directory) if f.endswith('.png')]
     for file in png_files:
         os.remove(os.path.join(directory, file))
-        print(f"{file} deleted.")
+        print(f"Deleted: {file}")
 
 
 def main():
-    # Get the current working directory
     directory = os.getcwd()
-
-    print('The directory is:', directory)
+    print(f"The directory is: {directory}")
 
     compress_fits_files(directory)
-    delete_uncompressed_images(directory)
     delete_png_files(directory)
 
 
