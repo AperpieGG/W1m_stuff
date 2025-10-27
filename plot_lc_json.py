b1f52b0c98b9829c34ec0d7c2e235f626bf1080c
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2025-10-27 12:58:05 +0000

    pass bin as argument

--- plot_lc_json.py
+++ plot_lc_json.py
@@ -14,7 +14,7 @@ def load_json(file_path):
         return json.load(file)
 
 
-def main(target_file):
+def main(target_file, bin):
     # Display any necessary images
     plot_images()
 
@@ -29,7 +29,7 @@ def main(target_file):
     print(f'Time length: {len(time)}, Flux length: {len(flux)}, Flux Error length: {len(flux_err)}')
 
     # Bin the data, for CMOS is 30 for 5 min and for CCD is 23 (10 + 3 seconds readout)
-    time_binned, flux_binned, fluxerr_binned = bin_by_time_interval(time, flux, flux_err, 0.1677)
+    time_binned, flux_binned, fluxerr_binned = bin_by_time_interval(time, flux, flux_err, bin)
     print(f'Binned Time length: {len(time_binned)}, Binned Flux length: {len(flux_binned)}, '
           f'Binned Flux Error length: {len(fluxerr_binned)}')
 
@@ -49,7 +49,8 @@ def main(target_file):
 if __name__ == "__main__":
     # Parse command-line arguments
     parser = argparse.ArgumentParser(description="Process Light Curve with Wotan")
-    parser.add_argument('--target_file', type=str, required=True, help="Path to the target JSON file")
+    parser.add_argument('target_file', type=str, required=True, help="Path to the target JSON file")
+    parser.add_argument('--bin', type=float, default=5, help='Binning interval in days (default: 0.1677 days ~ 24 min)')
     args = parser.parse_args()
 
     # Execute the main function with parsed arguments
